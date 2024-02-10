26c86b26d6afd17c6a8630dbeca1dbc5cf39ad59
John Russel Lacson <jelacson@up.edu.ph>
2024-02-11 03:02:41 +0800

    initial site

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      vermillion: "#e34234",
+    },
   },
   variants: {
     extend: {},
